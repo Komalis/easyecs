0d7f28e0d2aba9d0116977e0c7910a42c751fcfa
BONVARLET Benjamin <bbonvarlet@meilleursagents.com>
2023-08-05 15:09:43 +0200

    test(docker): check command is written correctly

--- easyecs/docker/__init__.py
+++ easyecs/docker/__init__.py
@@ -2,28 +2,34 @@ import subprocess
 from easyecs.helpers.common import parse_dict_with_env_var
 
 
+def docker_build_cmd(build, image_name):
+    dockerfile = build.dockerfile
+    target = build.target
+    if target:
+        target = f"--target {target}"
+    build_args = build.args
+    build_args = parse_dict_with_env_var(build_args)
+    build_args_str = " ".join(
+        [f'--build-arg {key}="{value}"' for key, value in build_args.items()]
+    )
+    build_cmd = f"docker build -t {image_name}"
+    if dockerfile:
+        build_cmd += f" -f {dockerfile}"
+    if target:
+        build_cmd += f" {target}"
+    if build.args:
+        build_cmd += f" {build_args_str}"
+    build_cmd += " ."
+    return build_cmd
+
+
 def build_docker_image(ecs_manifest):
     containers = ecs_manifest.task_definition.containers
     for container in containers:
         image_name = container.image
         build = container.build
         if build:
-            dockerfile = build.dockerfile
-            if dockerfile:
-                dockerfile = f"-f {dockerfile}"
-            target = build.target
-            if target:
-                target = f"--target {target}"
-            build_args = build.args
-            build_args = parse_dict_with_env_var(build_args)
-            build_args_str = " ".join(
-                [f'--build-arg {key}="{value}"' for key, value in build_args.items()]
-            )
-            build_cmd = f"docker build -t {image_name} {dockerfile}"
-            if target:
-                build_cmd += f" {target}"
-            build_cmd += f" {build_args_str}"
-            build_cmd += " ."
+            build_cmd = docker_build_cmd(build, image_name)
             res = subprocess.Popen(
                 build_cmd,
                 shell=True,
