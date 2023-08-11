1fa6bdf5f8478d993f4dd14a1e91d0f00dba78ac
BONVARLET Benjamin <bbonvarlet@meilleursagents.com>
2023-08-11 10:14:07 +0200

    chore(stack): cleaning error handling

--- easyecs/cloudformation/stack/delete.py
+++ easyecs/cloudformation/stack/delete.py
@@ -1,16 +1,65 @@
 import boto3
+from botocore.utils import ClientError
+from easyecs.cloudformation.client import get_client_cloudformation
 from easyecs.cloudformation.fetch import fetch_stack_url
+from easyecs.cloudformation.stack.update import (
+    wait_for_stack_create,
+    wait_for_stack_rollback,
+)
 from easyecs.helpers.color import Color
 
 from easyecs.helpers.loader import Loader
+
+
+def wait_for_stack_delete(stack_name: str):
+    """
+    Waits for the CloudFormation stack to be deleted.
+    """
+    client = get_client_cloudformation()
+    waiter = client.get_waiter("stack_delete_complete")
+    waiter.wait(StackName=stack_name)
+
+
+def handle_delete_error(e: ClientError, stack_name: str, loader: Loader):
+    """
+    Handles a CloudFormation stack update failure.
+    Depending on the error, either waits for a rollback, cancels an update,
+    or prints the error and breaks the loop.
+    """
+    message = e.response["Error"]["Message"]
+    if "UPDATE_IN_PROGRESS" in message:
+        cloudformation = boto3.resource("cloudformation")
+        stack = cloudformation.Stack(stack_name)
+        stack.cancel_update()
+        wait_for_stack_rollback(stack_name)
+        delete_stack(stack_name)
+        loader.stop()
+    elif "CREATE_FAILED" in message:
+        print(f"{Color.RED}Creation failed, please check CloudFormation.{Color.END}")
+        loader.stop()
+    elif "ROLLBACK_FAILED" in message:
+        print(f"{Color.RED}Rollback failed, please check CloudFormation.{Color.END}")
+        loader.stop()
+    elif "ROLLBACK_IN_PROGRESS" in message:
+        wait_for_stack_rollback(stack_name)
+        loader.stop()
+    elif "CREATE_IN_PROGRESS" in message:
+        wait_for_stack_create(stack_name)
+        loader.stop()
+    else:
+        loader.stop_error()
+        print(e)
+
+
+def delete_cloudformation_stack(stack_name: str):
+    client = get_client_cloudformation()
+    client.delete_stack(StackName=stack_name)
 
 
 def delete_stack(stack_name):
     """
     Deletes a CloudFormation stack with a given stack_name
     """
-    client = boto3.client("cloudformation")
-
     # Initialize the loader with appropriate messages
     loader = Loader(
         "Deleting CloudFormation stack:",
@@ -21,17 +70,9 @@ def delete_stack(stack_name):
     loader.start()
 
     try:
-        # Attempt to delete the CloudFormation stack
         loader.set_metadata(f"Cloudformation URL: {fetch_stack_url(stack_name)}")
-        client.delete_stack(StackName=stack_name)
-
-        # Initialize a waiter for the 'stack_delete_complete' event
-        waiter = client.get_waiter("stack_delete_complete")
-        waiter.wait(StackName=stack_name)
-        loader.stop()  # Stop the loader when the stack deletion is complete
-
-    except client.exceptions.StackNotFoundException:
-        # If the stack doesn't exist, stop the loader with an error and print a message
-        loader.stop_error()
-        print(f"{Color.RED}The CloudFormation stack does not exist!{Color.END}")
-        exit(-1)
+        delete_cloudformation_stack(stack_name)
+        wait_for_stack_delete(stack_name)
+        loader.stop()
+    except ClientError as e:
+        handle_delete_error(e, stack_name, loader)
